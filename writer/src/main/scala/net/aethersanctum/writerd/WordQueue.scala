2548d1b53b27ed1d9c98953aa1bda763290ba369
Ben Hardy <benhardy+github@gmail.com>
2016-01-27 20:41:42 -0500

    add corpus filtering

--- writer/src/main/scala/net/aethersanctum/writerd/WordQueue.scala
+++ writer/src/main/scala/net/aethersanctum/writerd/WordQueue.scala
@@ -1,10 +1,24 @@
 package net.aethersanctum.writerd
 
+import scala.annotation.tailrec
+
 case class Suggestion(word: Word)
 
 class WordQueue(val randomSource: Unit=>Double, val words: List[Word]) {
   def suggest = Suggestion(words((words.size * randomSource()).toInt))
 
+  def suggest(filter: Word=>Boolean): Suggestion = {
+    @tailrec
+    def withFilter(count:Int): Suggestion = {
+      val s = suggest
+      if (filter(s.word) || count <= 0) {
+        s
+      } else {
+        withFilter(count - 1)
+      }
+    }
+    withFilter(50)
+  }
 }
 
 object WordQueue {
--- writer/src/test/scala/net/aethersanctum/writerd/WordQueueSpec.scala
+++ writer/src/test/scala/net/aethersanctum/writerd/WordQueueSpec.scala
@@ -10,5 +10,15 @@ class WordQueueSpec extends BaseSpec {
       q.suggest should be === Suggestion("a")
       q.suggest should be === Suggestion("b")
     }
+
+    it("can do filtering") {
+      implicit val randomizer = TestUtil.mockRandom(0.01, 0.11, 0.21, 0.31, 0.41, 0.51, 0.61, 0.71, 0.81, 0.91)
+
+      val q = WordQueue("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k")
+
+      def filter(w:Word) = w.appearance == "g"
+
+      q.suggest(filter) should be === Suggestion("g")
+    }
   }
 }
